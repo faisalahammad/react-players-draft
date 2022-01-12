ad7b4e1540895cc0ef8f5181d09177bc81d64f4d
Faisal Ahammad <faisalahammad24@gmail.com>
2022-01-12 18:45:43 +0600

    RPD - Optimized codes, Drafted list added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { useEffect, useState } from 'react';
-import Draft from './components/Draft/Draft';
 import Header from './components/Header/Header';
 import Players from './components/Players/Players';
 
 function App() {
   const [players, setPlayers] = useState([]);
-  const handleAddPlayer = (newPlayer) => {
-    const newCart = [...players, newPlayer];
-    setPlayers(newCart);
+  const [team, setTeam] = useState([]);
+  const addMember = (name) => {
+    setTeam([...team, name]);
   }
-  const api_token = 'fmw6EBoZo07U5PPmvbxR8XUi0sflEYM4ecawmXTbs58i34hGQmjJzUAmK7pj';
 
+  const api_token = 'fmw6EBoZo07U5PPmvbxR8XUi0sflEYM4ecawmXTbs58i34hGQmjJzUAmK7pj';
   useEffect(() => {
     fetch(`https://soccer.sportmonks.com/api/v2.0/countries/462/players?api_token=${api_token}`, {
       method: 'Get',
@@ -29,32 +28,30 @@ function App() {
 
       <div className="container mt-5">
         <div className="row">
-          <div className="col-md-8 col-sm-12">
+          <div className="col-md-9 col-sm-12">
             <h2>Available Players</h2>
+
             {players.length > 0 && (
               <div className="row">
                 {players.map(player => (
-                  <div className='col-md-4 col-sm-6 col-xs-6 my-3' key={player.player_id}>
-                    <Players player={player} handleAddPlayer={handleAddPlayer } />
+                  <div className='col-lg-4 col-md-6 col-sm-6 my-3' key={player.player_id}>
+                    <Players player={player} addMember={addMember} />
                   </div>
                 ))}
               </div>
             )}
+
           </div>
-          <div className="col-md-4 col-sm-12">
+          <div className="col-md-3 col-sm-12">
             <h2>Drafted Players</h2>
 
-            {players.length > 0 && (
+            {team.length > 0 && (
               <ol>
-                {players.map(player => (
-                  <li key={player.player_id}>
-                    <Draft player={player} />
-                  </li>
+                {team.map((name, idx) => (
+                  <li key={idx}>{ name }</li>
                 ))}
               </ol>
             )}
-
-            {/* <Draft players={players} /> */}
 
           </div>
         </div>
--- src/components/Players/Players.js
+++ src/components/Players/Players.js
@@ -1,26 +1,32 @@
-import { faPlus } from '@fortawesome/free-solid-svg-icons';
+import { faEye, faPlus } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Players = (props) => {
-// console.log(props.player);
-  const player = props.player;
-  const playerPrice = Math.floor(Math.random() * (1000000 + player.player_id)) + 1;
+  const { display_name, image_path, height, weight, player_id, birthdate } = props.player;
+  const playerPrice = Math.floor(Math.random() * (10000000 + player_id)) + 1;
+  const [birthday, setBirthday] = useState('*****');
+  const showBDate = () => setBirthday(birthdate);
+  const addMember = props.addMember;
 
   return (
     <div className="card border-danger">
-      <img src={player.image_path} className="card-img-top" alt={player.display_name} />
+      <img src={image_path} className="card-img-top" alt={display_name} />
       <div className="card-body">
-        <h5 className="card-title">{player.display_name}</h5>
+        <h5 className="card-title">{display_name}</h5>
       </div>
       <ul className="list-group list-group-flush">
-        <li className="list-group-item"><b>Height:</b> {player.height}</li>
-        <li className="list-group-item"><b>Weight:</b> { player.weight }</li>
+        <li className="list-group-item"><b>Height:</b> {height}</li>
+        <li className="list-group-item"><b>Weight:</b> { weight }</li>
+        <li className="list-group-item"><b>Birthdate:</b> { birthday }</li>
         <li className="list-group-item"><b>Value:</b> ${ playerPrice.toLocaleString() }</li>
       </ul>
       <div className="card-body">
-        <button className="btn btn-danger" onClick={() => props.handleAddUser(player)}>
-          Sign { player.firstname } <FontAwesomeIcon icon={faPlus} />
+        <button className="btn btn-danger" onClick={()=>addMember(display_name)}>
+          Add <FontAwesomeIcon icon={faPlus} />
+        </button>
+        <button className="btn btn-warning ms-1" onClick={showBDate}>
+          Show <FontAwesomeIcon icon={faEye} />
         </button>
       </div>
     </div>
